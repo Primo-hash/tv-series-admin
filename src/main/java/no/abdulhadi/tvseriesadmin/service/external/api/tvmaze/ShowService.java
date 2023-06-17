8944b0ab008e0526fcaff1d180a7602c7ece01cf
Abdulhadi Al-Sayed <ahayed@yahoo.com>
2023-06-17 02:47:09 +0100

    Added backoff strategy for 429 http status code issue from tvmaze api

--- src/main/java/no/abdulhadi/tvseriesadmin/service/external/api/tvmaze/ShowService.java
+++ src/main/java/no/abdulhadi/tvseriesadmin/service/external/api/tvmaze/ShowService.java
@@ -1,16 +1,24 @@
 package no.abdulhadi.tvseriesadmin.service.external.api.tvmaze;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import no.abdulhadi.tvseriesadmin.exception.InjectorException;
 import no.abdulhadi.tvseriesadmin.model.dto.show_embed_episodes.ShowDTO;
 import no.abdulhadi.tvseriesadmin.service.external.api.WebFluxConfiguration;
 import no.abdulhadi.tvseriesadmin.service.injector.DTOInjector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.util.ArrayList;
 
 @Service
 public class ShowService {
+
+    private static final int BACKOFF_STRATEGY = 3;
+    private static final int BACKOFF_DURATION_MILLISECONDS = 1500;
+
+
     public static <T extends ShowDTO> ShowDTO getShowWithEmbed(String showName, String embed, Class<T> dtoClass) {
         WebClient client = WebClient.builder()
                 .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(WebFluxConfiguration.MAX_WEB_BUFFER_SIZE))
@@ -25,6 +33,7 @@ public class ShowService {
                         .build())
                 .retrieve()
                 .bodyToMono(String.class)
+                .retryWhen(Retry.backoff(BACKOFF_STRATEGY, Duration.ofMillis(BACKOFF_DURATION_MILLISECONDS)))
                 .block();
 
         try {
